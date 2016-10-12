943b79d6e1c87679881384a4e15cf3f1c8e3455e
odoku <m.onogawa@gmail.com>
2016-10-12 14:35:15 +0900

    認証の基本的な部分を実装。

--- example/index.js
+++ example/index.js
@@ -2,6 +2,25 @@ const path = require('path'),
       koala = require('../lib/mr-koala'),
       logger = require('koa-logger');
 
+
+koala.auth.ignores = ['/users/:id'];
+koala.auth.method = 'basic';
+koala.auth.handler = function(username, cb) {
+  if (username === 'hoge') {
+    return cb(null, {name: 'ohtani'}, 'fuga');
+  }
+  return cb(null, false);
+};
+
+// koala.auth.method = 'digest';
+// koala.auth.handler = function(username, password, cb) {
+//   if (username === 'hoge' && password === 'fuga') {
+//     return cb(null, {name: 'ohtani'});
+//   }
+//   return cb(null, false);
+// }
+
+
 const mr = koala(
   path.dirname(__filename) + '/api.raml',
   {resources: path.dirname(__filename) + '/resources'}
